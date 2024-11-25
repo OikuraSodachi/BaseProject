e412a450bfcf8dd351ff586d1fb79627934b9614
Todokanai <bh951201@gmail.com>
2024-11-25 10:41:22 +0900

    code cleanup

--- app/src/main/java/com/todokanai/baseproject/abstracts/BaseRecyclerViewHolder.kt
+++ app/src/main/java/com/todokanai/baseproject/abstracts/BaseRecyclerViewHolder.kt
@@ -3,9 +3,7 @@ package com.todokanai.baseproject.abstracts
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-open class BaseRecyclerViewHolder<E:Any>(val view: View):RecyclerView.ViewHolder(view) {
+abstract class BaseRecyclerViewHolder<E:Any>(val view: View):RecyclerView.ViewHolder(view) {
 
-    open fun onInit(item:E){
-
-    }
+    abstract fun onInit(item:E)
 }
