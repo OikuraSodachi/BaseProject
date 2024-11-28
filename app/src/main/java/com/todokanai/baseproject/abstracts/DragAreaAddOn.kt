6ad1a08e107b5359b9ac15d939eaeae31cb33ddf
Todokanai <bh951201@gmail.com>
2024-11-29 08:15:34 +0900

    drag 해서 범위 지정 기능 진행중 AreaSelectAddOn.kt -> DragAreaAddOn.kt 으로 rename
    BaseAreaAddOn.kt -> BaseDragAreaAddOn.kt 으로 rename

--- app/src/main/java/com/todokanai/baseproject/abstracts/DragAreaAddOn.kt
+++ app/src/main/java/com/todokanai/baseproject/abstracts/DragAreaAddOn.kt
@@ -5,27 +5,27 @@ import android.view.View
 /**
  * Unstable
  *
- * @param view area to apply areaSelect feature
+ * @param view area to apply drag area feature
  *
  * **/
-class AreaSelectAddOn(
+class DragAreaAddOn(
     private val view: View,
     val onArea:(Float,Float,Float,Float)->Unit
-):BaseAreaAddOn(view) {
+):BaseDragAreaAddOn(view) {
 
-    /** dragSelect 동작 개시시에 **/
+    /** drag area 동작 개시시에 **/
     override fun onStartArea(value: Boolean) {
         super.onStartArea(value)
         /** Todo: RecyclerView 등의 경우, scroll 활성화 / 비활성화를 여기서 해야할듯? **/
 
     }
 
-    /** dragSelect 동작 도중에 **/
+    /** drag area 동작 도중에 **/
     override fun onMovement(startX: Float, startY: Float, endX: Float, endY: Float){
         // TODO() 대충 범위 이내의 item을 선택된 목록에 누적하여 추가
     }
 
-    /** 시작지점과 끝 지점 좌표 사이의 구간 (Windows의 마우스 클릭-드래그 범위선택에 해당) 를 parameter로서 가져옴 **/
+    /** 시작지점과 끝 지점 좌표 사이의 구간 (Windows의 마우스 클릭-드래그 범위지정에 해당) 를 parameter로서 가져옴 **/
     override fun onGetAreaResult(startX: Float, startY: Float, endX: Float, endY: Float) {
         onGetAreaResult(startX,startY,endX,endY)
     }
