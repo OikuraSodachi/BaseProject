258f88a29ca40f043569036c454ffcf78ad81a5c
Todokanai <bh951201@gmail.com>
2024-11-29 14:53:13 +0900

    drag 해서 범위 지정 기능 진행중 Selection 라이브러리 쓰기로 다시 결정함

--- app/src/main/java/com/todokanai/baseproject/tools/independent/unstable/DragAreaAddOn.kt
+++ app/src/main/java/com/todokanai/baseproject/tools/independent/unstable/DragAreaAddOn.kt
@@ -2,14 +2,8 @@ package com.todokanai.baseproject.tools.independent.unstable
 
 import android.view.View
 
-/**
- * @param view [View] to apply dragArea feature
- *
- * **/
-class DragAreaAddOn(
-    private val view: View,
-    val onGetArea:(Float,Float,Float,Float)->Unit
-) {
+/** class for dragArea feature (Windows의 클릭-드래그 범위지정에 해당) **/
+class DragAreaAddOn() {
     /** drag 활성화 여부 **/
     private var enabled:Boolean = false
 
@@ -17,8 +11,11 @@ class DragAreaAddOn(
     private var startX : Float = 0f
     private var startY : Float = 0f
 
+    /** a longClickListener preset for [startDragArea] **/
     val longClickListener = View.OnLongClickListener {
+        /*
         isStartReady = false
+        onDragStart()
         it.setOnTouchListener { view, motionEvent ->
             if (isStartReady == false) {
                 startX = motionEvent.x
@@ -27,25 +24,56 @@ class DragAreaAddOn(
             }
             false
         }
+        false
+         */
+        startDragArea(it)
+        false
+    }
+
+    /** dragArea event를 시작하는 callback
+     *
+     * @param view View to invoke event from
+     * **/
+    fun startDragArea(view: View){
+        isStartReady = false
         enabled = true
-        false
+        view.setOnTouchListener { _, motionEvent ->
+            if (isStartReady == false) {
+                startX = motionEvent.x
+                startY = motionEvent.y
+                isStartReady = true
+            }
+            false
+        }
     }
 
-    val onTouchListener = View.OnTouchListener { view, motionEvent ->
-        if(enabled == true && motionEvent.action == 1){
-
-            onGetArea(startX,startY,motionEvent.x,motionEvent.y)
-
-            enabled = false
+    private val onTouchListener = View.OnTouchListener { view, motionEvent ->
+        if(enabled){
+            if(motionEvent.action == 1){
+                onGetArea(startX,startY,motionEvent.x,motionEvent.y)
+                enabled = false
+            }else{
+                onMove(startX,startY,motionEvent.x,motionEvent.y)
+            }
         }
         false
 
     }
 
-    fun add(){
-        view.run{
-            setOnLongClickListener(longClickListener)
-            setOnTouchListener(onTouchListener)
-        }
+    /**
+     * drag 동작 적용 가능한 범위 (view) 지정
+     * @param view View Area to enable drag event
+     * **/
+    fun setAreaRange(view: View){
+        view.setOnTouchListener(onTouchListener)
+    }
+    /** called during dragArea event**/
+    fun onMove(startX:Float,startY:Float,endX:Float,endY:Float){
+
+    }
+
+    /** called when dragArea event has ended **/
+    fun onGetArea(startX:Float,startY:Float,endX:Float,endY:Float){
+
     }
 }
