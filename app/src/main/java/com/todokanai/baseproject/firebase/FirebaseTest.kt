d1af9e9feaed733bef017731303193c6b72cc955
Todokanai <bh951201@gmail.com>
2025-01-17 18:51:17 +0900

    firebase 부분 추가

--- app/src/main/java/com/todokanai/baseproject/firebase/FirebaseTest.kt
+++ app/src/main/java/com/todokanai/baseproject/firebase/FirebaseTest.kt
@@ -3,7 +3,10 @@ package com.todokanai.baseproject.firebase
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-/** FCM messaging based on Firebase Realtime Database **/
+/** FCM messaging based on Firebase Realtime Database
+ *
+ * google-services.json file required
+ * **/
 class FirebaseTest(database:FirebaseDatabase) {
 
     private val ref = database.reference
