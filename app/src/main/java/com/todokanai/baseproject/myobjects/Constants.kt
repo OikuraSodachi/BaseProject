2e2bf015af26a5c43b0a2bc1fa4f299c09d8e55f
Todokanai <bh951201@gmail.com>
2025-01-20 11:15:32 +0900

    BaseNotification 진행중

--- app/src/main/java/com/todokanai/baseproject/myobjects/Constants.kt
+++ app/src/main/java/com/todokanai/baseproject/myobjects/Constants.kt
@@ -3,4 +3,7 @@ package com.todokanai.baseproject.myobjects
 object Constants {
 
     const val PERMISSION_REQUEST_CODE : Int = 111
+
+    const val CHANNEL_ID = "Todokanai_BaseProject_ID"
+    const val NOTIFICATION_CHANNEL_NAME : String = "Todokanai_BaseProject_Channel"
 }
