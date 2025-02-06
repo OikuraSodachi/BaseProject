c8149f8b45240c1b5f152c79ab39496871426fc6
Todokanai <bh951201@gmail.com>
2025-02-07 07:53:44 +0900

    DataStoreRepository 개선

--- app/src/main/java/com/todokanai/baseproject/repository/DataStoreRepository.kt
+++ app/src/main/java/com/todokanai/baseproject/repository/DataStoreRepository.kt
@@ -9,11 +9,12 @@ import javax.inject.Singleton
 @Singleton
 class DataStoreRepository @Inject constructor(appContext:Context):MyDataStore(appContext){
     companion object{
-        val DATASTORE_STRING = stringPreferencesKey("datastore_sort_by")
+        private val DATASTORE_STRING = stringPreferencesKey("datastore_sort_by")
+        private const val DATASTORE_STRING_DEFAULT_VALUE : String = "default"
     }
     suspend fun saveString(value:String) = DATASTORE_STRING.save(value)
-
     suspend fun getString() = DATASTORE_STRING.value()
-
+    suspend fun getStringNonNull() = DATASTORE_STRING.notNullValue(DATASTORE_STRING_DEFAULT_VALUE)
     val stringFlow = DATASTORE_STRING.flow()
+    val stringFlowNotNull = DATASTORE_STRING.notNullFlow(DATASTORE_STRING_DEFAULT_VALUE)
 }
