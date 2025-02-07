6b2477611c1475302a1d249e6d9503a685c75d59
Todokanai <bh951201@gmail.com>
2025-02-08 07:10:29 +0900

    code cleanup

--- app/src/main/java/com/todokanai/baseproject/repository/DataStoreRepository.kt
+++ app/src/main/java/com/todokanai/baseproject/repository/DataStoreRepository.kt
@@ -10,11 +10,11 @@ import javax.inject.Singleton
 class DataStoreRepository @Inject constructor(appContext:Context):MyDataStore(appContext){
     companion object{
         private val DATASTORE_STRING = stringPreferencesKey("datastore_sort_by")
-        private const val DATASTORE_STRING_DEFAULT_VALUE : String = "default"
+        private val STRING_DEFAULT_VALUE : String = "default"
     }
     suspend fun saveString(value:String) = DATASTORE_STRING.save(value)
     suspend fun getString() = DATASTORE_STRING.value()
-    suspend fun getStringNonNull() = DATASTORE_STRING.notNullValue(DATASTORE_STRING_DEFAULT_VALUE)
+    suspend fun getStringNonNull() = DATASTORE_STRING.notNullValue(STRING_DEFAULT_VALUE)
     val stringFlow = DATASTORE_STRING.flow()
-    val stringFlowNotNull = DATASTORE_STRING.notNullFlow(DATASTORE_STRING_DEFAULT_VALUE)
+    val stringFlowNotNull = DATASTORE_STRING.notNullFlow(STRING_DEFAULT_VALUE)
 }
