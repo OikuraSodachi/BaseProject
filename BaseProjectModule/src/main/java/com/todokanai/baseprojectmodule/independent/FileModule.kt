4ec8bcf4a662f1db5a4bf038f71051ddab63e0fb
Todokanai <bh951201@gmail.com>
2025-02-08 07:14:47 +0900

    code cleanup

--- BaseProjectModule/src/main/java/com/todokanai/baseprojectmodule/independent/FileModule.kt
+++ BaseProjectModule/src/main/java/com/todokanai/baseprojectmodule/independent/FileModule.kt
@@ -13,13 +13,13 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 import java.io.File
 
-class FileModule(defaultPath:String) {
+class FileModule(defaultPath:File) {
 
     /** 현재 보고있는 Directory
      *
      *  Primary Key(?)
      * **/
-    private val _currentPath = MutableStateFlow(File(defaultPath))
+    private val _currentPath = MutableStateFlow(defaultPath)
     val currentPath : Flow<File>
         get() = _currentPath
 
