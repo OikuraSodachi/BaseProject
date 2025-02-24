597dc6ecb991b2892dfc990553202790e446398a
Todokanai <bh951201@gmail.com>
2025-02-24 16:41:36 +0900

    SavedStateFlow.kt 추가

---
+++ app/src/main/java/com/todokanai/baseproject/data/dataclass/SavedStateFlow.kt
@@ -0,0 +1,29 @@
+package com.todokanai.baseproject.data.dataclass
+
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/** MutableStateFlow designed for remembering last known value
+ *
+ * @param initialValue initial value of this stateflow (usually from database)
+ * @param saveValue save value to database **/
+data class SavedStateFlow<Type:Any>(
+    val initialValue:Type,
+    val saveValue:(value:Type)->Unit
+): StateFlow<Type> {
+    private val stateFlowInstance = MutableStateFlow<Type>(initialValue)
+
+    fun setValueTest(value:Type){
+        stateFlowInstance.value = value
+        saveValue(value)
+    }
+
+    override val replayCache: List<Type>
+        get() = stateFlowInstance.replayCache
+    override val value: Type
+        get() = stateFlowInstance.value
+    override suspend fun collect(collector: FlowCollector<Type>): Nothing {
+        stateFlowInstance.collect(collector)
+    }
+}
