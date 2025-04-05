9c9115972b13c7b9db9cb24fdfed3362b14c1ef6
Todokanai <bh951201@gmail.com>
2025-04-05 18:58:51 +0900

    Selection 라이브러리 기반으로 유지 결정
    Safe

--- app/src/main/java/com/todokanai/baseproject/abstracts/multiselectrecyclerview/MultiSelectRecyclerAdapter.kt
+++ app/src/main/java/com/todokanai/baseproject/abstracts/multiselectrecyclerview/MultiSelectRecyclerAdapter.kt
@@ -15,27 +15,27 @@ import androidx.recyclerview.widget.RecyclerView
  * position:Int == key:Long .toInt()
  *
  * selectionTracker.hasSelection() -> return false if selection is a empty list **/
-abstract class MultiSelectRecyclerAdapter<E:Any,VH:RecyclerView.ViewHolder>(
-    diffUtil:DiffUtil.ItemCallback<E>
-): ListAdapter<E, VH>(diffUtil) {
+abstract class MultiSelectRecyclerAdapter<E : Any, VH : RecyclerView.ViewHolder>(
+    diffUtil: DiffUtil.ItemCallback<E>
+) : ListAdapter<E, VH>(diffUtil) {
     private lateinit var selectionTracker: SelectionTracker<Long>
-    abstract val selectionId:String
+    abstract val selectionId: String
 
     /** callback for when selection is enabled/disabled **/
     @CallSuper
-    open fun onSelectionModeEnabled(enabled:Boolean){
-        if(!enabled) {
+    open fun onSelectionModeEnabled(enabled: Boolean) {
+        if (!enabled) {
             selectionTracker.clearSelection()
         }
         println("onSelectionModeEnabled: $enabled")
     }
 
-    fun selectedItemKeys():Set<Long>{
+    fun selectedItemKeys(): Set<Long> {
         return selectionTracker.selection.toSet()
     }
 
     /** @return set of selected items **/
-    fun selectedItems():Set<E>{
+    fun selectedItems(): Set<E> {
         return selectedItemKeys().map {
             currentList[it.toInt()]
         }.toSet()
@@ -68,18 +68,18 @@ abstract class MultiSelectRecyclerAdapter<E:Any,VH:RecyclerView.ViewHolder>(
         )
     }
 
-    override fun getItemId(position: Int):Long{
+    override fun getItemId(position: Int): Long {
         return position.toLong()
     }
 
     /** workaround fix for selection being cleared on touching outside
      *
      * [issueTracker link](https://issuetracker.google.com/issues/177046288#comment7) **/
-    private fun selectionBugFix(recyclerView: RecyclerView){
+    private fun selectionBugFix(recyclerView: RecyclerView) {
         recyclerView.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
             override fun onInterceptTouchEvent(rv: RecyclerView, event: MotionEvent): Boolean {
                 val down = event.actionMasked == MotionEvent.ACTION_DOWN
-                if(!down || !selectionTracker.hasSelection()) {
+                if (!down || !selectionTracker.hasSelection()) {
                     return false //Don't intercept, otherwise you break scrolling
                 }
 
@@ -87,8 +87,8 @@ abstract class MultiSelectRecyclerAdapter<E:Any,VH:RecyclerView.ViewHolder>(
                 return view == null
             }
 
-            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) { }
-            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) { }
+            override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
         })
     }
 }
