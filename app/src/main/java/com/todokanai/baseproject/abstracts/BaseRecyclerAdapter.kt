61f9c5794524463d7874349a1b40f7510f55ef03
Todokanai <bh951201@gmail.com>
2025-04-06 16:51:44 +0900

    ListAdapter, DiffUtil 기반으로 BaseRecyclerAdapter 재작성
    Safe

---
+++ app/src/main/java/com/todokanai/baseproject/abstracts/BaseRecyclerAdapter.kt
@@ -0,0 +1,11 @@
+package com.todokanai.baseproject.abstracts
+
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseRecyclerAdapter<E:Any,VH:RecyclerView.ViewHolder>(
+    diffUtil: DiffUtil.ItemCallback<E>
+) : ListAdapter<E, VH>(diffUtil){
+
+}
