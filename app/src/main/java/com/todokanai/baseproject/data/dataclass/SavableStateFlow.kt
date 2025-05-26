e8e146be30bb0817139917b5a17d7bd865c8b2f6
Todokanai <bh951201@gmail.com>
2025-05-26 16:12:34 +0900

    SavableStateFlow refactor.
    Safe

--- app/src/main/java/com/todokanai/baseproject/data/dataclass/SavableStateFlow.kt
+++ app/src/main/java/com/todokanai/baseproject/data/dataclass/SavableStateFlow.kt
@@ -1,29 +1,19 @@
 package com.todokanai.baseproject.data.dataclass
 
-import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 
-/** MutableStateFlow designed for remembering last known value
+/** MutableStateFlow designed for saving last known value
  * @author Oikura Sodachi
- * @param initialValue initial value of this stateflow (usually from database)
+ * @param originalStateFlow original MutableStateFlow
  * @param saveValue save value to database **/
 data class SavableStateFlow<Type:Any>(
-    private val initialValue:Type,
+    private val originalStateFlow:MutableStateFlow<Type>,
     private val saveValue:(value:Type)->Unit
-): StateFlow<Type> {
-    private val stateFlowInstance = MutableStateFlow<Type>(initialValue)
-
-    fun setValue(value:Type){
-        stateFlowInstance.value = value
-        saveValue(value)
-    }
-
-    override val replayCache: List<Type>
-        get() = stateFlowInstance.replayCache
-    override val value: Type
-        get() = stateFlowInstance.value
-    override suspend fun collect(collector: FlowCollector<Type>): Nothing {
-        stateFlowInstance.collect(collector)
-    }
+): MutableStateFlow<Type> by originalStateFlow {
+    override var value: Type
+        get() = originalStateFlow.value
+        set(newValue) {
+            originalStateFlow.value = newValue
+            saveValue
+        }
 }
