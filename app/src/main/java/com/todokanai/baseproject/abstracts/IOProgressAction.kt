63da32996dc54edf502739440c6c6796067fd3ac
Todokanai <bh951201@gmail.com>
2025-06-16 07:43:33 +0900

    IOProgressAction 추가.
    Safe

---
+++ app/src/main/java/com/todokanai/baseproject/abstracts/IOProgressAction.kt
@@ -0,0 +1,34 @@
+package com.todokanai.baseproject.abstracts
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.InputStream
+
+interface IOProgressAction{
+
+    /** @param outputFileInProgress file that is currently in progress
+     * @param bytesRead bytes read from input stream
+     * @param bytesWritten total bytes written on [outputFileInProgress] **/
+    suspend fun byteProgressCallback(outputFileInProgress:File, bytesRead:Long, bytesWritten:Long)
+
+    /** start writing **/
+    suspend fun doIOStreamWithProgress(
+        inputStream:InputStream,
+        outputFile: File,
+        bufferSize: Int = 8192*100
+    ) = withContext(Dispatchers.IO){
+        val outputStream = outputFile.outputStream()
+        var bytesWritten: Long = 0
+        val buffer = ByteArray(bufferSize)
+        var bytesRead: Int
+
+        while (inputStream.read(buffer).also { bytesRead = it } > 0) {
+            outputStream.write(buffer, 0, bytesRead)
+            bytesWritten += bytesRead
+            byteProgressCallback(outputFile,bytesRead.toLong(),bytesWritten)
+        }
+        inputStream.close()
+        outputStream.close()
+    }
+}
