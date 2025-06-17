b57fcadde431b2be2d68e039fc9f3e886ed502a4
Todokanai <bh951201@gmail.com>
2025-06-17 10:35:12 +0900

    Notifications refactor.
    Safe

--- app/src/main/java/com/todokanai/baseproject/tools/Notifications.kt
+++ app/src/main/java/com/todokanai/baseproject/tools/Notifications.kt
@@ -1,26 +1,21 @@
 package com.todokanai.baseproject.tools
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.content.Context
 import androidx.annotation.DrawableRes
+import androidx.core.app.NotificationCompat
 
-open class Notifications(
-    private val notificationManager: NotificationManager,
-    private val channel: NotificationChannel,
-    @DrawableRes private val smallIcon: Int
-){
-    /** Must be called on initialization **/
-    fun createNotificationChannel() = notificationManager.createNotificationChannel(channel)
+/** 여기서 NotificationManager 가 보이면 안됨 **/
+class Notifications(@DrawableRes private val smallIcon: Int){
 
-    fun displayNotification(context: Context, title:String, contentText:String, notificationId:Int = 1) {
-        val notification = Notification.Builder(context, channel.id)
+    fun basicNotification(
+        context: Context,
+        title: String?,
+        message: String?,
+        channelId:String
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(context,channelId)
             .setSmallIcon(smallIcon)
             .setContentTitle(title)
-            .setContentText(contentText)
-            .build()
-
-        notificationManager.notify(notificationId, notification)
+            .setContentText(message)
     }
 }
